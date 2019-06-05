270ebd4b703aa33aa59939a7fa3f94cea6da2cd3
DJP <djpade@gmail.com>
2019-06-05 13:54:03 -0400

    First Commit

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  padelude = pkgs.haskellPackages.mkDerivation {
+    pname = "padelude";
+    version = "2018-01-12";
+    license = "GPLv3";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "qfjp";
+      repo = "padelude";
+      rev = "c549b22ef4f2883d43dd3828910136c8147ff51c";
+      sha256 = "0n7xjbb0s722rk0y7knj7vq1qv7zglz4rpx7qxzplwzbn4lff96s";
+    };
+
+    buildDepends = [
+      pkgs.haskellPackages.protolude
+      pkgs.haskellPackages.set-monad
+    ];
+  };
+in pkgs.haskellPackages.mkDerivation {
+  pname = "template-nix";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = with pkgs.haskellPackages; [ base padelude ];
+  executableHaskellDepends = with pkgs.haskellPackages; [ base padelude ];
+  executableSystemDepends = [];
+  homepage = "https://github.com/qfjp/template";
+  description = "A short description of the template";
+  license = with pkgs; stdenv.lib.licenses.gpl3;
+}
